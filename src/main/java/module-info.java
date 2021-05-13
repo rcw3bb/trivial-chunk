f3e6d5e314320e3d4af197059db34ebe99339c70
ron <ron@ronella.xyz>
2021-05-14 09:20:15 +1200

    Module info update.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,5 @@ module xyz.ronella.casual.trivial {
     exports xyz.ronella.trivial.decorator;
     exports xyz.ronella.trivial.functional;
     exports xyz.ronella.trivial.handy;
+    exports xyz.ronella.trivial.handy.impl;
 }
