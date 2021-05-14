aba77f1093d4fd62cf364c0bcf72d1ea43b1d436
ron <ron@ronella.xyz>
2021-05-14 19:12:35 +1200

    Add javadocs.

--- src/main/java/xyz/ronella/trivial/handy/Require.java
+++ src/main/java/xyz/ronella/trivial/handy/Require.java
@@ -2,8 +2,22 @@ package xyz.ronella.trivial.handy;
 
 import java.util.Objects;
 
+/**
+ * A handy class for requiring objects.
+ *
+ * @author Ron Webb
+ * @since 2.3.0
+ */
 public class Require {
 
+    /**
+     * A method that checks if all the parameters were not null.
+     *
+     * @param param The required first parameter to check.
+     * @param params The other parameters to check.
+     *
+     * @throws RequireAllException This will be thrown with at least one of the parameters throws an NPE.
+     */
     public static void all(Object param, Object ... params) throws RequireAllException {
         try {
             Objects.requireNonNull(param);
