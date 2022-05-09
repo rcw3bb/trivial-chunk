11de4afa47d24d5b5bb5ba30cef0e795ffc67ee6
Ron Webb <ron@ronella.xyz>
2022-05-09 13:28:07 +1200

    Add javadoc comment.

--- src/main/java/xyz/ronella/trivial/handy/RequireAllException.java
+++ src/main/java/xyz/ronella/trivial/handy/RequireAllException.java
@@ -1,7 +1,15 @@
 package xyz.ronella.trivial.handy;
 
+/**
+ * Thrown with one of argument in Require.objects is null.
+ *
+ * @author Ron Webb
+ */
 public class RequireAllException extends RuntimeException {
 
+    /**
+     * Creates an instance of RequireAllException.
+     */
     public RequireAllException() {
         super();
     }
