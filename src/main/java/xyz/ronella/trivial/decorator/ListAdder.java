de3f31c88075bf217141957dbb8be0050638f6f4
Ron Webb <ron@ronella.xyz>
2022-05-23 11:23:59 +1200

    Add ListAdder implementation.

---
+++ src/main/java/xyz/ronella/trivial/decorator/ListAdder.java
@@ -0,0 +1,81 @@
+package xyz.ronella.trivial.decorator;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
+/**
+ * A class the wraps a List to have add methods with condition.
+ *
+ * @param <TYPE_ELEMENT>
+ *
+ * @since 2.10.0
+ * @author Ron Webb
+ */
+public class ListAdder<TYPE_ELEMENT> {
+
+    private final List<TYPE_ELEMENT> list;
+
+    /**
+     * Creates an instance of ListAdder
+     * @param list An instance of List to wrap.
+     */
+    public ListAdder(List<TYPE_ELEMENT> list) {
+        this.list = list;
+    }
+
+    /**
+     * Add element to a wrapped list.
+     *
+     * @param when The logic that must return true to add the element.
+     * @param element Specified in List.add
+     * @return Specified in List.add
+     */
+    public boolean add(BooleanSupplier when, TYPE_ELEMENT element) {
+        if (when.getAsBoolean()) {
+            return list.add(element);
+        }
+        return false;
+    }
+
+    /**
+     * Add elements to a wrapped list.
+     *
+     * @param when The logic that must return true to add the elements.
+     * @param elements Specified in List.addAll
+     * @return Specified in List.addAll
+     */
+    public boolean addAll(BooleanSupplier when, Collection<? extends TYPE_ELEMENT> elements) {
+        if (when.getAsBoolean()) {
+            return list.addAll(elements);
+        }
+        return false;
+    }
+
+    /**
+     * Add element to a specific index of the wrapped list
+     * @param when The logic that must return true to add the element.
+     * @param index Specified in List.add
+     * @param element Specified in List.add
+     */
+    public void add(BooleanSupplier when, int index, TYPE_ELEMENT element) {
+        if (when.getAsBoolean()) {
+            list.add(index, element);
+        }
+    }
+
+    /**
+     * Add elements to a specified index of the wrapped list.
+     * @param when The logic that must return true to add the elements.
+     * @param index Specified in List.add
+     * @param elements Specified in List.add
+     * @return Specified in List.addAll
+     */
+    public boolean addAll(BooleanSupplier when, int index, Collection<? extends TYPE_ELEMENT> elements) {
+        if (when.getAsBoolean()) {
+            return list.addAll(index, elements);
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/xyz/ronella/trivial/decorator/ListAdderTest.java
@@ -0,0 +1,93 @@
+package xyz.ronella.trivial.decorator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ListAdderTest {
+
+    @Test
+    public void addWhenTrue() {
+        var expected = Arrays.asList("Test");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.add(()-> true, expected.get(0));
+
+        assertArrayEquals(expected.toArray(), testList.toArray());
+    }
+
+    @Test
+    public void addWhenFalse() {
+        var expected = Arrays.asList("Test");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.add(()-> false, expected.get(0));
+
+        assertEquals(0, testList.size());
+    }
+
+    @Test
+    public void addIdxWhenTrue() {
+        var expected = Arrays.asList("Test");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.add(()-> true, 0, expected.get(0));
+
+        assertArrayEquals(expected.toArray(), testList.toArray());
+    }
+
+    @Test
+    public void addIdxWhenFalse() {
+        var expected = Arrays.asList("Test");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.add(()-> false, 0, expected.get(0));
+
+        assertEquals(0, testList.size());
+    }
+
+    @Test
+    public void addAllWhenTrue() {
+        var expected = Arrays.asList("Test1", "Test2");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.addAll(()-> true, expected);
+
+        assertArrayEquals(expected.toArray(), testList.toArray());
+    }
+
+    @Test
+    public void addAllWhenFalse() {
+        var expected = Arrays.asList("Test1", "Test2");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.addAll(()-> false, expected);
+
+        assertEquals(0, testList.size());
+    }
+
+    @Test
+    public void addAllIdxWhenTrue() {
+        var expected = Arrays.asList("Test1", "Test2");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.addAll(()-> true, 0, expected);
+
+        assertArrayEquals(expected.toArray(), testList.toArray());
+    }
+
+    @Test
+    public void addAllIdxWhenFalse() {
+        var expected = Arrays.asList("Test1", "Test2");
+        var testList = new ArrayList<String>();
+        var adder = new ListAdder<>(testList);
+        adder.addAll(()-> false, 0, expected);
+
+        assertEquals(0, testList.size());
+    }
+
+}
