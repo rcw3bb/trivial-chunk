2943f9c4e234f6e862f4c50588b7b683abf12584
Ron Webb <ron@ronella.xyz>
2022-06-01 09:16:54 +1200

    Add when argument.

--- src/main/java/xyz/ronella/trivial/decorator/ListAdder.java
+++ src/main/java/xyz/ronella/trivial/decorator/ListAdder.java
@@ -3,6 +3,7 @@ package xyz.ronella.trivial.decorator;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 /**
  * A class the wraps a List to have add methods with condition.
@@ -39,12 +40,38 @@ public class ListAdder<TYPE_ELEMENT> {
     }
 
     /**
+     * Add element to a wrapped list.
+     *
+     * @param when The logic that must return true to add the element.
+     * @param generateElement The logic that will generate the element.
+     * @return Specified in List.add
+     */
+    public boolean add(BooleanSupplier when, Supplier<TYPE_ELEMENT> generateElement) {
+        if (when.getAsBoolean()) {
+            var element = generateElement.get();
+            return list.add(element);
+        }
+        return false;
+    }
+
+    /**
      * Add element to a wrapped list. Added for convenience.
      *
      * @param element Specified in List.add
      * @return Specified in List.add
      */
     public boolean add(TYPE_ELEMENT element) {
+        return list.add(element);
+    }
+
+    /**
+     * Add element to a wrapped list.
+     *
+     * @param generateElement The logic that will generate the element.
+     * @return Specified in List.add
+     */
+    public boolean add(Supplier<TYPE_ELEMENT> generateElement) {
+        var element = generateElement.get();
         return list.add(element);
     }
 
@@ -63,12 +90,38 @@ public class ListAdder<TYPE_ELEMENT> {
     }
 
     /**
+     * Add elements to a wrapped list.
+     *
+     * @param when The logic that must return true to add the elements.
+     * @param generateElements The logic that will generate the elements.
+     * @return Specified in List.addAll
+     */
+    public boolean addAll(BooleanSupplier when, Supplier<Collection<? extends TYPE_ELEMENT>> generateElements) {
+        if (when.getAsBoolean()) {
+            var elements = generateElements.get();
+            return list.addAll(elements);
+        }
+        return false;
+    }
+
+    /**
      * Add elements to a wrapped list. Added for convenience.
      *
      * @param elements Specified in List.addAll
      * @return Specified in List.addAll
      */
     public boolean addAll(Collection<? extends TYPE_ELEMENT> elements) {
+        return list.addAll(elements);
+    }
+
+    /**
+     * Add elements to a wrapped list.
+     *
+     * @param generateElements The logic that will generate the elements.
+     * @return Specified in List.addAll
+     */
+    public boolean addAll(Supplier<Collection<? extends TYPE_ELEMENT>> generateElements) {
+        var elements = generateElements.get();
         return list.addAll(elements);
     }
 
@@ -85,11 +138,34 @@ public class ListAdder<TYPE_ELEMENT> {
     }
 
     /**
+     * Add element to a specific index of the wrapped list.
+     * @param when The logic that must return true to add the element.
+     * @param index Specified in List.add
+     * @param generateElement The logic that will generate the element.
+     */
+    public void add(BooleanSupplier when, int index, Supplier<TYPE_ELEMENT> generateElement) {
+        if (when.getAsBoolean()) {
+            var element = generateElement.get();
+            list.add(index, element);
+        }
+    }
+
+    /**
      * Add element to a specific index of the wrapped list. Added for convenience.
      * @param index Specified in List.add
      * @param element Specified in List.add
      */
     public void add(int index, TYPE_ELEMENT element) {
+        list.add(index, element);
+    }
+
+    /**
+     * Add element to a specific index of the wrapped list.
+     * @param index Specified in List.add
+     * @param generateElement The logic that will generate the element.
+     */
+    public void add(int index, Supplier<TYPE_ELEMENT> generateElement) {
+        var element = generateElement.get();
         list.add(index, element);
     }
 
@@ -108,12 +184,38 @@ public class ListAdder<TYPE_ELEMENT> {
     }
 
     /**
+     * Add elements to a specified index of the wrapped list.
+     * @param when The logic that must return true to add the elements.
+     * @param index Specified in List.add
+     * @param generateElements The logic that will generate the elements.
+     * @return Specified in List.addAll
+     */
+    public boolean addAll(BooleanSupplier when, int index, Supplier<Collection<? extends TYPE_ELEMENT>> generateElements) {
+        if (when.getAsBoolean()) {
+            var elements = generateElements.get();
+            return list.addAll(index, elements);
+        }
+        return false;
+    }
+
+    /**
      * Add elements to a specified index of the wrapped list. Added for convenience.
      * @param index Specified in List.add
      * @param elements Specified in List.add
      * @return Specified in List.addAll
      */
     public boolean addAll(int index, Collection<? extends TYPE_ELEMENT> elements) {
+        return list.addAll(index, elements);
+    }
+
+    /**
+     * Add elements to a specified index of the wrapped list. Added for convenience.
+     * @param index Specified in List.add
+     * @param generateElements The logic that will generate the elements.
+     * @return Specified in List.addAll
+     */
+    public boolean addAll(int index, Supplier<Collection<? extends TYPE_ELEMENT>> generateElements) {
+        var elements = generateElements.get();
         return list.addAll(index, elements);
     }
 
