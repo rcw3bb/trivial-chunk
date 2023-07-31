2c249f8d73c05319789b805086ff115c8f519196
Ron Webb <ron@ronella.xyz>
2023-08-01 09:34:11 +1200

    Make whenThen just a sub-implementation of core functional interfaces.

--- src/main/java/xyz/ronella/trivial/functional/WhenThen.java
+++ src/main/java/xyz/ronella/trivial/functional/WhenThen.java
@@ -1,6 +1,7 @@
 package xyz.ronella.trivial.functional;
 
 import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
 
 /**
  * Must have the implementation to process a task when the condition was met.
@@ -10,11 +11,22 @@ import java.util.function.BooleanSupplier;
  * @since 2.16.0
  */
 @FunctionalInterface
-public interface WhenThen {
+public interface WhenThen extends Consumer<BooleanSupplier> {
 
     /**
      * Must hold the logic to process a task when the condition was met.
+     * The Then part of WhenThen is the actual implementation of when.
+     *
      * @param condition The condition that must be true to do the task.
      */
     void when(BooleanSupplier condition);
+
+    /**
+     * Accepts just redirect the call to implementation.
+     * @param condition the input argument
+     */
+    @Override
+    default void accept(final BooleanSupplier condition) {
+        when(condition);
+    }
 }
--- src/main/java/xyz/ronella/trivial/functional/WhenThenReturn.java
+++ src/main/java/xyz/ronella/trivial/functional/WhenThenReturn.java
@@ -1,6 +1,7 @@
 package xyz.ronella.trivial.functional;
 
 import java.util.function.BooleanSupplier;
+import java.util.function.Function;
 
 /**
  * Must have the implementation to generate the output when the condition was met.
@@ -10,12 +11,24 @@ import java.util.function.BooleanSupplier;
  * @since 2.16.0
  */
 @FunctionalInterface
-public interface WhenThenReturn<TYPE_OUTPUT> {
+public interface WhenThenReturn<TYPE_OUTPUT> extends Function<BooleanSupplier, TYPE_OUTPUT> {
 
     /**
      * Must hold the logic to generate the output when the condition was met.
+     * The Then part of WhenThen is the actual implementation of when.
+     *
      * @param condition The condition that must be true to generate the output.
      */
     TYPE_OUTPUT when(BooleanSupplier condition);
+
+    /**
+     * Redirect the call to when implementation.
+     * @param supplier the function argument
+     * @return The output of when implementation.
+     */
+    @Override
+    default TYPE_OUTPUT apply(final BooleanSupplier supplier) {
+        return when(supplier);
+    }
 
 }
