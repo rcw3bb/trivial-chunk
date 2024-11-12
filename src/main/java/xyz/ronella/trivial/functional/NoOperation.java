39da3ac26b8708cac891e4c91b9a23e7cf351193
Ron Webb <ron@ronella.xyz>
2024-11-12 19:46:00 +1300

    Update NoOperation for Checked functional interfaces.

--- src/main/java/xyz/ronella/trivial/functional/NoOperation.java
+++ src/main/java/xyz/ronella/trivial/functional/NoOperation.java
@@ -4,9 +4,8 @@ import java.util.function.*;
 
 /**
  * A do nothing implementation of some functional interfaces.
- *
+ * 
  * @author Ron Webb
- *
  * @since 2.2.0
  */
 public final class NoOperation {
@@ -25,6 +24,19 @@ public final class NoOperation {
     }
 
     /**
+     * An implementation of CheckedConsumer that does nothing.
+     *
+     * @param <T> The type of the argument of the consumer.
+     * @param <X> The type of the exception.
+     *
+     * @return A do nothing implementation of CheckedConsumer.
+     * @since 2.21.0
+     */
+    public static <T, X extends Exception> CheckedConsumer<T, X> checkedConsumer() {
+        return arg1 -> {};
+    }
+
+    /**
      * An implementation of BiConsumer that does nothing.
      *
      * @param <T1> The type of the first argument of the BiConsumer.
@@ -33,6 +45,20 @@ public final class NoOperation {
      * @return A do nothing implementation of BiConsumer.
      */
     public static <T1, T2> BiConsumer<T1, T2> biConsumer() {
+        return (arg1, arg2) -> {};
+    }
+
+    /**
+     * An implementation of CheckedBiConsumer that does nothing.
+     *
+     * @param <T1> The type of the first argument of the BiConsumer.
+     * @param <T2> The type of the second argument of the BiConsumer.
+     * @param <X> The type of the exception.
+     *
+     * @return A do nothing implementation of CheckedBiConsumer.
+     * @since 2.21.0
+     */
+    public static <T1, T2, X extends Exception> CheckedBiConsumer<T1, T2, X> checkedBiConsumer() {
         return (arg1, arg2) -> {};
     }
 
@@ -46,6 +72,18 @@ public final class NoOperation {
     }
 
     /**
+     * An implementation of CheckedSink that does nothing.
+     *
+     * @param <X> The type of the exception.
+     *
+     * @return A do nothing implementation of CheckedSink.
+     * @since 2.21.0
+     */
+    public static <X extends Exception> CheckedSink<X> checkedSink() {
+        return () -> {};
+    }
+
+    /**
      * An implementation of Supplier that does nothing.
      *
      * @param <T> The type of output that the supplier will generate.
@@ -53,6 +91,19 @@ public final class NoOperation {
      * @return Always return null.
      */
     public static <T> Supplier<T> supplier() {
+        return () -> null;
+    }
+
+    /**
+     * An implementation of CheckedSupplier that does nothing.
+     *
+     * @param <T> The type of output that the supplier will generate.
+     * @param <X> The type of the exception.
+     *
+     * @return Always return null.
+     * @since 2.21.0
+     */
+    public static <T, X extends Exception> CheckedSupplier<T, X> checkedSupplier() {
         return () -> null;
     }
 
@@ -69,7 +120,21 @@ public final class NoOperation {
     }
 
     /**
-     * An implementation of Predicate that does nothing but must have a specific boolean output.
+     * An implementation of CheckedPredicate that does nothing but must have a specific boolean output.
+     *
+     * @param output The desired output of the do nothing.
+     * @param <T> The type of argument that the predicate will work on.
+     * @param <X> The type of the exception.
+     *
+     * @return The value of the output parameter.
+     * @since 2.21.0
+     */
+    public static <T, X extends Exception> CheckedPredicate<T, X> checkedPredicate(final Boolean output) {
+        return (arg) -> output;
+    }
+
+    /**
+     * An implementation of BiPredicate that does nothing but must have a specific boolean output.
      *
      * @param output The desired output of the do nothing.
      * @param <T1> The type of first argument that the predicate will work on.
@@ -82,6 +147,21 @@ public final class NoOperation {
     }
 
     /**
+     * An implementation of CheckedBiPredicate that does nothing but must have a specific boolean output.
+     *
+     * @param output The desired output of the do nothing.
+     * @param <T1> The type of first argument that the predicate will work on.
+     * @param <T2> The type of second argument that the predicate will work on.
+     * @param <X> The type of the exception.
+     *
+     * @return The value of the output parameter.
+     * @since 2.21.0
+     */
+    public static <T1, T2, X extends Exception> CheckedBiPredicate<T1, T2, X> checkedBiPredicate(final Boolean output) {
+        return (arg1, arg2) -> output;
+    }
+
+    /**
      * An implementation of Function that does nothing.
      * @param <T> The type of the argument to work on.
      * @param <R> The return type.
@@ -89,6 +169,19 @@ public final class NoOperation {
      * @return Always return null.
      */
     public static <T,R> Function<T,R> function() {
+        return (arg) -> null;
+    }
+
+    /**
+     * An implementation of CheckedFunction that does nothing.
+     * @param <T> The type of the argument to work on.
+     * @param <R> The return type.
+     * @param <X> The type of the exception.
+     *
+     * @return Always return null.
+     * @since 2.21.0
+     */
+    public static <T,R, X extends Exception> CheckedFunction<T,R, X> checkedFunction() {
         return (arg) -> null;
     }
 
@@ -107,6 +200,20 @@ public final class NoOperation {
     }
 
     /**
+     * An implementation of CheckedFunction that does nothing but produce default output.
+     * @param <T> The type of the argument to work on.
+     * @param <R> The return type.
+     * @param <X> The type of the exception.
+     * @param output The desired output of type R.
+     *
+     * @return The value of the output parameter.
+     * @since 2.21.0
+     */
+    public static <T,R, X extends Exception> CheckedFunction<T,R, X> checkedFunction(final R output) {
+        return (arg) -> output;
+    }
+
+    /**
      * An implementation of Function that does nothing and just return what passed.
      * @param <T> The type of the argument of instance of R to work on.
      * @param <R> The return type.
@@ -120,6 +227,19 @@ public final class NoOperation {
     }
 
     /**
+     * An implementation of CheckedFunction that does nothing and just return what passed.
+     * @param <T> The type of the argument of instance of R to work on.
+     * @param <R> The return type.
+     * @param <X> The type of the exception.
+     *
+     * @return The value of the output parameter.
+     * @since 2.21.0
+     */
+    public static <T extends R,R, X extends Exception> CheckedFunction<T,R, X> checkedFunctionPassThru() {
+        return (arg) -> arg;
+    }
+
+    /**
      * An implementation of BiFunction that does nothing.
      * @param <T1> The type of the first argument to work on.
      * @param <T2> The type of the second argument to work on.
@@ -129,6 +249,20 @@ public final class NoOperation {
      * @return Always return null.
      */
     public static <T1, T2, R> BiFunction<T1, T2, R> biFunction() {
+        return (arg1, arg2) -> null;
+    }
+
+    /**
+     * An implementation of CheckedBiFunction that does nothing.
+     * @param <T1> The type of the first argument to work on.
+     * @param <T2> The type of the second argument to work on.
+     * @param <R> The return type.
+     * @param <X> The type of the exception.
+     *
+     * @return Always return null.
+     * @since 2.21.0
+     */
+    public static <T1, T2, R, X extends Exception> CheckedBiFunction<T1, T2, R, X> checkedBiFunction() {
         return (arg1, arg2) -> null;
     }
 
@@ -148,6 +282,21 @@ public final class NoOperation {
     }
 
     /**
+     * An implementation of CheckedBiFunction that does nothing but produce default output.
+     * @param <T1> The type of the first argument to work on.
+     * @param <T2> The type of the second argument to work on.
+     * @param <R> The return type.
+     * @param <X> The type of the exception.
+     * @param output The desired output of type R.
+     *
+     * @return The value of the output parameter.
+     * @since 2.21.0
+     */
+    public static <T1, T2, R, X extends Exception> CheckedBiFunction<T1, T2, R, X> checkedBiFunction(final R output) {
+        return (arg1, arg2) -> output;
+    }
+
+    /**
      * An implementation of BiFunction that does nothing and just return argument 1.
      * @param <T1> The type of the first argument of instance of R to work on.
      * @param <T2> The type of the second argument to work on.
@@ -162,6 +311,20 @@ public final class NoOperation {
     }
 
     /**
+     * An implementation of CheckedBiFunction that does nothing and just return argument 1.
+     * @param <T1> The type of the first argument of instance of R to work on.
+     * @param <T2> The type of the second argument to work on.
+     * @param <R> The return type.
+     * @param <X> The type of the exception.
+     *
+     * @return The value of the output parameter.
+     * @since 2.21.0
+     */
+    public static <T1 extends R, T2, R, X extends Exception> CheckedBiFunction<T1, T2, R, X> checkedBiFunctionArg1PassThru() {
+        return (arg1, arg2) -> arg1;
+    }
+
+    /**
      * An implementation of BiFunction that does nothing and just return argument 2.
      * @param <T1> The type of the first argument to work on.
      * @param <T2> The type of the second argument of instance of R to work on.
@@ -172,6 +335,20 @@ public final class NoOperation {
      * @since 2.11.0
      */
     public static <T1, T2 extends R, R> BiFunction<T1, T2, R> biFunctionArg2PassThru() {
+        return (arg1, arg2) -> arg2;
+    }
+
+    /**
+     * An implementation of CheckedBiFunction that does nothing and just return argument 2.
+     * @param <T1> The type of the first argument to work on.
+     * @param <T2> The type of the second argument of instance of R to work on.
+     * @param <R> The return type.
+     * @param <X> The type of the exception.
+     *
+     * @return The value of the output parameter.
+     * @since 2.21.0
+     */
+    public static <T1, T2 extends R, R, X extends Exception> CheckedBiFunction<T1, T2, R, X> checkedBiFunctionArg2PassThru() {
         return (arg1, arg2) -> arg2;
     }
 }
--- src/test/java/xyz/ronella/trivial/functional/NoOperationTest.java
+++ src/test/java/xyz/ronella/trivial/functional/NoOperationTest.java
@@ -110,4 +110,106 @@ public class NoOperationTest {
         BiPredicate<String, String> biPredicate = NoOperation.biPredicate(Boolean.FALSE);
         assertFalse(biPredicate.test("test", "test2"));
     }
+
+    @Test
+    public void noopCheckedConsumerTest() throws Exception {
+        var sb = new StringBuilder();
+        CheckedConsumer<String, Exception> consumer = (arg1) -> sb.append(arg1);
+        consumer = NoOperation.checkedConsumer();
+        consumer.checkedAccept("Test");
+
+        assertEquals("", sb.toString());
+    }
+
+    @Test
+    public void noopCheckedBiConsumerTest() throws Exception {
+        var sb = new StringBuilderAppender();
+        CheckedBiConsumer<String, String, Exception> biConsumer = (arg1, arg2) -> sb.append(arg1, arg2);
+        biConsumer = NoOperation.checkedBiConsumer();
+        biConsumer.checkedAccept("Test1", "Test2");
+
+        assertEquals("", sb.toString());
+    }
+
+    @Test
+    public void noopCheckedSinkTest() throws Exception {
+        var sb = new StringBuilder();
+        CheckedSink<Exception> sink = () -> sb.append("Test");
+        sink = NoOperation.checkedSink();
+        sink.checkedPlummet();
+
+        assertEquals("", sb.toString());
+    }
+
+    @Test
+    public void noopCheckedSupplierTest() throws Exception {
+        CheckedSupplier<String, Exception> supplier = NoOperation.checkedSupplier();
+        assertNull(supplier.checkedGet());
+    }
+
+    @Test
+    public void noopCheckedPredicateTrueTest() throws Exception {
+        CheckedPredicate<String, Exception> predicate = NoOperation.checkedPredicate(Boolean.TRUE);
+        assertTrue(predicate.checkedTest("test"));
+    }
+
+    @Test
+    public void noopCheckedPredicateFalseTest() throws Exception {
+        CheckedPredicate<String, Exception> predicate = NoOperation.checkedPredicate(Boolean.FALSE);
+        assertFalse(predicate.checkedTest("test"));
+    }
+
+    @Test
+    public void noopCheckedFunctionTest() throws Exception {
+        CheckedFunction<String, String, Exception> function = NoOperation.checkedFunction();
+        assertNull(function.checkedApply("test"));
+    }
+
+    @Test
+    public void noopCheckedFunctionOutputTest() throws Exception {
+        CheckedFunction<String, String, Exception> function = NoOperation.checkedFunction("output");
+        assertEquals("output", function.checkedApply("test"));
+    }
+
+    @Test
+    public void noopCheckedFunctionPassThru() throws Exception {
+        CheckedFunction<String, String, Exception> function = NoOperation.checkedFunctionPassThru();
+        assertEquals("output", function.checkedApply("output"));
+    }
+
+    @Test
+    public void noopCheckedBiFunctionTest() throws Exception {
+        CheckedBiFunction<String, String, String, Exception> biFunction = NoOperation.checkedBiFunction();
+        assertNull(biFunction.checkedApply("test", "test2"));
+    }
+
+    @Test
+    public void noopCheckedBiFunctionOutputTest() throws Exception {
+        CheckedBiFunction<String, String, String, Exception> biFunction = NoOperation.checkedBiFunction("output");
+        assertEquals("output", biFunction.checkedApply("test", "test2"));
+    }
+
+    @Test
+    public void noopCheckedBiFunctionArg1PassThru() throws Exception {
+        CheckedBiFunction<String, String, String, Exception> biFunction = NoOperation.checkedBiFunctionArg1PassThru();
+        assertEquals("arg1", biFunction.checkedApply("arg1", "arg2"));
+    }
+
+    @Test
+    public void noopCheckedBiFunctionArg2PassThru() throws Exception {
+        CheckedBiFunction<String, String, String, Exception> biFunction = NoOperation.checkedBiFunctionArg2PassThru();
+        assertEquals("arg2", biFunction.checkedApply("arg1", "arg2"));
+    }
+
+    @Test
+    public void noopCheckedBiPredicateTrueTest() throws Exception {
+        CheckedBiPredicate<String, String, Exception> biPredicate = NoOperation.checkedBiPredicate(Boolean.TRUE);
+        assertTrue(biPredicate.checkedTest("test", "test2"));
+    }
+
+    @Test
+    public void noopCheckedBiPredicateFalseTest() throws Exception {
+        CheckedBiPredicate<String, String, Exception> biPredicate = NoOperation.checkedBiPredicate(Boolean.FALSE);
+        assertFalse(biPredicate.checkedTest("test", "test2"));
+    }
 }
