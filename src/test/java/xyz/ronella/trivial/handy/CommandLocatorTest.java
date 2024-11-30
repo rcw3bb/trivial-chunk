765ca1f719e2bfee677862e9ea7ecc3fd42cf1f1
Ron Webb <ron@ronella.xyz>
2024-11-30 14:24:02 +1300

    Add test to CommandLocator to return only the first path when multiple paths were returned by the CommandProcessor.

--- src/test/java/xyz/ronella/trivial/handy/CommandLocatorTest.java
+++ src/test/java/xyz/ronella/trivial/handy/CommandLocatorTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Optional;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class CommandLocatorTest {
     @Test
@@ -60,6 +61,23 @@ public class CommandLocatorTest {
     @Test
     public void findNullAsFile() {
         assertThrows(ObjectRequiredException.class, () -> CommandLocator.locateAsFile(null));
+    }
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
+    @SuppressWarnings("unchecked")
+    public void commandLocatorFirstPathOnly() {
+        final var eol = OSType.WINDOWS.getEOL().eol();
+        final var locatorOutput = String.format("C:\\dev\\apps\\graalvm-jdk-21.0.3+7.1\\bin\\java.exe%s" +
+                        "C:\\Program Files\\Common Files\\Oracle\\Java\\javapath\\java.exe%s", eol, eol);
+
+        try (var mockCommandProcessor = mockStatic(CommandProcessor.class)) {
+            mockCommandProcessor.when(() -> CommandProcessor.process(any(Function.class), any(ICommandArray.class)))
+                    .thenReturn(Optional.of(locatorOutput));
+
+            final var command = CommandLocator.locateAsFile("java");
+            assertEquals("C:\\dev\\apps\\graalvm-jdk-21.0.3+7.1\\bin\\java.exe", command.get().getPath());
+        }
     }
 
 }
